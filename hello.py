ca85e8888b5f073f7a4a10cdc4b6d2dc3bbd95eb
John Paguirigan <johnjaroldxvi@gmail.com>
2020-04-18 22:09:40 -0600

    learning python!

---
+++ hello.py
@@ -0,0 +1,9 @@
+# print('hello world!')
+# # this is just a comment
+# name = input("What is your name:")
+# age = input("And your age is:")
+# print(name, 'you are', age)
+
+radius = input("Enter the radius of your circle (m):")
+area = 3.142 * int(radius)**2
+print("The area of your circle is:", area)
---
+++ if-statements.py
@@ -0,0 +1,15 @@
+# age = int(input("Enter your age:"))
+
+# if age < 10:
+#     print("You are young, little one")
+# elif age < 40:
+#     print("The fire in you is strong, strange one...")
+# else:
+#     print("What the heck are you..?")
+
+meaty = input("Do you eat meat? (y/n):")
+
+if meaty == 'y':
+    print('You are a meaty one!')
+else:
+    print("Don't even talk to me!")
