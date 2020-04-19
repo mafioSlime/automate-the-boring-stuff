ddc622ca824824a4a86cdb0a4f7cc7f692b2507c
John Paguirigan <johnjaroldxvi@gmail.com>
2020-04-18 23:19:05 -0600

    added while loop

--- for-loops.py
+++ for-loops.py
@@ -8,9 +8,33 @@ fruits = ['orange', 'pear', 'mango', 'strawberry']
 # for fruit in fruits[1:3]:
 #     print(fruit)
 
-for fruit in fruits:
-    if fruit == "mango":
-        print(f"{fruit} is delicious!")
+# for fruit in fruits:
+#     if fruit == "mango":
+#         print(f"{fruit} is delicious!")
+#         break
+#     else:
+#         print(fruit)
+
+# WHILE LOOPS
+
+age = 25
+num = 0
+
+# while num < age:
+#     if num == 0:
+#         num += 1
+#         continue
+#     if num % 2 == 0:
+#         print(num)
+#     if num > 10:
+#         break
+#     num += 1
+
+while num < age:
+    if num == 2:
+        print(f"{num} is my favorite number")
+    if num % 2 == 0:
+        print(num)
+    if num > 10:
         break
-    else:
-        print(fruit)
+    num += 1
