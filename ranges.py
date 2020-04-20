ae37a1a0065b3191fea285d5b27ed784a25b207a
John Paguirigan <johnjaroldxvi@gmail.com>
2020-04-19 22:51:01 -0600

    added range

---
+++ ranges.py
@@ -0,0 +1,31 @@
+# for n in range(5):
+#     print(n)
+
+# for n in range(3, 10):
+#     print(n)
+
+
+# for n in range(0, 20, 4):
+#     print(n)
+
+burgers = ["beef", "chicken", "veggie", "supreme", "double"]
+
+# for n in range(len(burgers)):
+#     print(n, burgers[n])
+
+# for n in range(len(burgers) - 1, -1, -1):
+#     print(n, burgers[n])
+
+# for burger in range(len(burgers)):
+#     print(burger, burgers[burger])
+
+
+# OWN EXAMPLE
+
+# fruits = ['mango', 'banana', 'strawberry', 'avocado']
+
+# for fruit in range(len(fruits)):
+#     print(fruit, fruits[fruit])
+
+# for n in range(5, 50, 10):
+#     print(n)
