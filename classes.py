20710f236c484310cf09323da864535eb9423cd3
John Paguirigan <johnjaroldxvi@gmail.com>
2020-04-21 21:32:43 -0600

    added init function

--- classes.py
+++ classes.py
@@ -1,18 +1,25 @@
 class Planet:
-    def __init__(self):
-        self.name = "Hoth"
-        self.radius = 2000000
-        self.gravity = 5.5
-        self.system = "Hoth System"
+    def __init__(self, name, radius, gravity, system):
+        self.name = name
+        self.radius = radius
+        self.gravity = gravity
+        self.system = system
 
     def orbit(self):
         return f"{self.name} is orbiting in the {self.system}"
 
 
-hoth = Planet()
+hoth = Planet("Hoth", 200000, 5.5, "Hoth System")
 
 print(f"This is the planet name: {hoth.name}")
 print(f"This is the radius: {hoth.radius}")
 print(f"this is the gravity: {hoth.gravity}")
 
 print(hoth.orbit())
+
+jura = Planet("Jura", 150000, 9.0, "Jura System")
+
+print(f"Planet Name: {jura.name}")
+print(f"Radius: {jura.radius}")
+print(f"Gravity: {jura.gravity}")
+print(f"System: {jura.system}")
